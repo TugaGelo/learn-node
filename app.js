40629d3ddb70f7812cf2957ed7f28eacb01c5567
Tugaga <rolandtugaoen013@gmail.com>
2025-04-03 19:46:45 +0800

    Create test file for Node.js GET POST DELETE study

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.set('view engine', 'ejs');
 
 //middleware and static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}));
 app.use(morgan('dev'));
 
 // //mongoose and mongo sandbox routes
@@ -105,10 +106,46 @@ app.get('/blogs', (req, res) => {
         })
 })
 
+app.post('/blogs', (req, res) => {
+    // console.log(req.body);
+    const blog = new Blog(req.body);
+
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs');
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
+
 // //redirects
 // app.get('/about-me', (req, res) => {
 //     res.redirect('/about');
 // })
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    // console.log(id);
+    Blog.findById(id)
+        .then(result => {
+            res.render('details', {blog: result, title: 'Blog Details'});
+        })
+    .catch(err => {
+        console.log(err);
+    });
+})
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+
+    Blog.findByIdAndDelete(id)
+        .then(result => {
+            res.json({ redirect: '/blogs' })
+        })
+        .catch(err => {
+            console.log(err);
+        })
+})
 
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create'});
